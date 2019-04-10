9bc0368aef976340b66701da2231f9122e1b61e5
eroha186 <dimon.s186@mail.ru>
2019-04-10 12:28:18 +0300

    Модальное окно + подгрузка видео в него

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function bindSlodeToggle(trigger, boxBody, content, openClass) { // Функци
     'element': document.querySelector(trigger),
     'active': false
   };
+
   const box = document.querySelector(boxBody),
     boxContent = document.querySelector(content);
 
@@ -61,6 +62,7 @@ function switchMode() {
 }
 
 let night = false;
+
 switcher.addEventListener('change', () => {
   switchMode();
 });
@@ -89,12 +91,90 @@ more.addEventListener('click', () => {
        ${data[1][i]}
     </div>
     <div class="videos__item-views">
-        ${data[3][i]}
+        ${data[2][i]}
     </div>
     `;
     videos__wrapper.appendChild(card);
-    setTimeout(() =>{
+    setTimeout(() => {
       card.classList.remove('videos__item-active')
     }, 10);
+    bindNewModal(card);
+  }
+
+  sliceTitle('.videos__item-descr', 70);
+});
+
+function sliceTitle(selector, count) {
+  document.querySelectorAll(selector).forEach(item => {
+    item.textContent.trim();
+    if (item.textContent.length < count) {
+      return;
+    } else {
+      const str = item.textContent.slice(0, count + 1) + "...";
+      item.textContent = str;
+    }
+  });
+}
+
+sliceTitle('.videos__item-descr', 70);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  player.stopVideo();
+}
+
+function bindModal(cards) {
+  cards.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = item.getAttribute('data-url');
+      loadVideo(id);      
+      openModal();
+    });
+  });
+}
+
+bindModal(videos);
+
+function bindNewModal(card) {
+  card.addEventListener('click', (e) => {
+    e.preventDefault();
+    const id = card.getAttribute('data-url');
+    loadVideo(id);      
+    openModal();
+  });
+}
+
+modal.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('modal__body')) {
+    closeModal();
   }
 });
+
+function createVideo() {
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  setTimeout(() => {
+    player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: 'M7lc1UVf-VE',
+    });
+  }, 300);
+}
+
+createVideo();
+
+function loadVideo(id) {
+  player.loadVideoById({
+    'videoId' : `${id}` 
+  });
+}
